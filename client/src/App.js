73b78d66a0fa3a49b2f0a3e02d600cb305bb1ed0
K L <kaiqi.liang@stonybrook.edu>
2023-04-03 00:03:49 -0400

    can't delete database entry so changed tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
 
 const api = axios.create({
   baseURL: 'https://shapeshifter-api.onrender.com'
-   //baseURL: "http://10.245.200.126:3000"
+  // baseURL: "http://localhost:5000"
 })
 export default function App() {
   const [firstName, setFirstName] = useState("");
@@ -16,72 +16,135 @@ export default function App() {
   const [loginPassword, setLoginPassword] = useState("");
   const [loginMsg, setLoginMsg] = useState("");
 
-  const onSignup = async () => { 
-    await api.post("/auth/signup", {firstName: firstName, lastName: lastName, username: username, email:email, password: password})
+  const [rememberPassUsername, setRememberPassUsername] = useState("");
+  const [rememberPassEmail, setRememberPassEmail] = useState("");
+  const [rememberPass, setRememberPass] = useState("");
+
+  const [changePassUsername, setChangePassUsername] = useState("");
+  const [changePassPassword, setChangePassPassword] = useState("");
+  const [changePassNewPassword, setChangePassNewPassword] = useState("");
+  const [changePass, setChangePass] = useState("");
+
+  const onSignup = async () => {
+    await api.post("/auth/signup", { firstName: firstName, lastName: lastName, username: username, email: email, password: password })
+      .then(function (res) {
+        console.log(res);
+        if (res.data.error) {
+          setSignupMsg(res.data.message)
+        } else {
+          setSignupMsg("Sign up success")
+        }
+      });
+  }
+  const onLogin = async () => {
+    await api.post("/auth/login", { email: loginEmail, password: loginPassword })
+      .then(function (res) {
+        console.log(res);
+        if (res.data.error) {
+          setLoginMsg("login error")
+        } else {
+          setLoginMsg("login success")
+        }
+      });
+  }
+
+  const onRememberPassword = async () => {
+    console.log(rememberPassEmail);
+    console.log(rememberPassUsername);
+    await api.get(`/auth/remember-password?email=${rememberPassEmail}&username=${rememberPassUsername}`)
     .then(function (res) {
       console.log(res);
-      if(res.data.error){
-        setSignupMsg("Sign up error")
-      }else{
-        setSignupMsg("Sign up success")
+      if (res.data.error) {
+        setRememberPass("remember password error")
+      } else {
+        setRememberPass("pass: " + res.data.password)
       }
     });
   }
-  const onLogin = async () => { 
-    await api.post("/auth/login", {email:loginEmail, password: loginPassword})
+
+  const onChangePassword = async () => {
+    console.log(changePassUsername);
+    console.log(changePassPassword);
+    console.log(changePassNewPassword);
+    await api.put("/auth/change-password", 
+    { username: changePassUsername, password: changePassPassword, newPassword: changePassNewPassword })
     .then(function (res) {
       console.log(res);
-      if(res.data.error){
-        setLoginMsg("login error")
-      }else{
-        setLoginMsg("login success")
+      if (res.data.error) {
+        setChangePass("change password error")
+      } else {
+        setChangePass("Password Changed!")
       }
-    });;
+    });
   }
 
   return (
     <div>
-      
       <p>Sign Up!</p>
-      <form>
-            <div>
-                <label>First Name:</label>
-                <input type="text" id="sign-up-firstname" value = {firstName} onChange = {(e) => setFirstName(e.target.value)}/>
-            </div>
-            <div>
-                <label>Last Name:</label>
-                <input type="text" id="sign-up-lastname" value = {lastName} onChange = {(e) => setLastName(e.target.value)} />
-            </div>
-            <div>
-                <label>Username:</label>
-                <input type="text" id="sign-up-username" value = {username} onChange = {(e) => setUsername(e.target.value)}/>
-            </div>
-            <div>
-                <label>Email:</label>
-                <input type="text" id="sign-up-email" value = {email} onChange = {(e) => setEmail(e.target.value)}/>
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="text" id="sign-up-password" value = {password} onChange = {(e) => setPassword(e.target.value)}/>
-            </div>
-            <input type="button" id="sign-up-submit" value="Signup" onClick={onSignup}/>
-            {signupMsg}
-     </form>
+      <div>
+        <label>First Name:</label>
+        <input type="text" id="sign-up-firstname" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input type="text" id="sign-up-lastname" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      </div>
+      <div>
+        <label>Userame:</label>
+        <input type="text" id="sign-up-username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="text" id="sign-up-email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" id="sign-up-password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <input type="button" id="sign-up-submit" value="Signup" onClick={onSignup} />
+      {signupMsg}
 
-        <p>Login!</p>
-        <form>
-            <div>
-                <label>Email:</label>
-                <input type="text" id="login-email" value = {loginEmail} onChange = {(e) => setLoginEmail(e.target.value)}/>
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="text" id="login-password" value = {loginPassword} onChange = {(e) => setLoginPassword(e.target.value)}/>
-            </div>
-            <input type="button" id="sign-up-submit" value="Login" onClick={onLogin}/>
-            {loginMsg}
-      </form>
-            
+
+      <p>Login!</p>
+      <div>
+        <label>Email:</label>
+        <input type="text" id="login-email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" id="login-password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
+      </div>
+      <input type="button" id="log-in-submit" value="Login" onClick={onLogin} />
+      {loginMsg}
+
+      <p>Forgot Password?</p>
+      <div>
+        <label>Email:</label>
+        <input type="text" id="remember-email" value={rememberPassEmail} onChange={(e) => setRememberPassEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Username:</label>
+        <input type="text" id="remember-username" value={rememberPassUsername} onChange={(e) => setRememberPassUsername(e.target.value)} />
+      </div>
+      <input type="button" id="remember-pass-submit" value="Remember Password" onClick={onRememberPassword} />
+      {rememberPass}
+
+      <p>Change Password</p>
+      <div>
+        <label>Username:</label>
+        <input type="text" id="change-username" value={changePassUsername} onChange={(e) => setChangePassUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" id="change-password" value={changePassPassword} onChange={(e) => setChangePassPassword(e.target.value)} />
+      </div>
+      <div>
+        <label>New Password:</label>
+        <input type="text" id="change-new-password" value={changePassNewPassword} onChange={(e) => setChangePassNewPassword(e.target.value)} />
+      </div>
+      <input type="button" id="change-pass-submit" value="Change Password" onClick={onChangePassword} />
+      {changePass}
+
     </div>
   )
 }
--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -1,31 +1,16 @@
 import axios from "axios";
 describe('welcome page', () => {
   beforeEach(function () {
-    //cy.task('db:seed')
-    // ...
+   // cy.request('DELETE', 'https://shapershifter.onrender.com', {firstname: 'John'})
   })
   it('successfully loads', () => {
     cy.visit('https://shapershifter.onrender.com') 
   })
   
-  it('the user does an incomplete signup', () => {
-    cy.visit('https://shapershifter.onrender.com')
-    cy.get('form')
-      cy.get('[id=sign-up-firstname]').type('John')
-      cy.get('[id=sign-up-lastname]').type('Smith')
-      cy.get('[id=sign-up-username]').type('smithy')
-      cy.get('[id=sign-up-email]').type('john.smith@gmail.com')
-      //.submit()
-    cy.get('[value="Signup"]').click()
-    cy.contains('div', 'Sign up error')
-   // cy.should("contain", "Sign up error")
-   
 
-  })
-
-  it('the user signs up for an account', () => {
+  it('existing user signs up for an account', () => {
     cy.visit('https://shapershifter.onrender.com')
-    cy.get('form')
+    //cy.get('form')
       cy.get('[id=sign-up-firstname]').type('John')
       cy.get('[id=sign-up-lastname]').type('Smith')
       cy.get('[id=sign-up-username]').type('smithy')
@@ -33,13 +18,13 @@ describe('welcome page', () => {
       cy.get('[id=sign-up-password]').type('thisisapassword123')
 
     cy.get('[value="Signup"]').click()
-    cy.contains('div', 'Sign up success')
+    cy.contains('div', 'user exists')
   })
 
 
   it('the user logs in to an account', () => {
     cy.visit('https://shapershifter.onrender.com')
-    cy.get('form')
+    //cy.get('form')
       cy.get('[id=login-email]').type('john.smith@gmail.com')
       cy.get('[id=login-password]').type('thisisapassword123')
 
@@ -47,5 +32,19 @@ describe('welcome page', () => {
     cy.get('[value="Login"]').click()
     cy.contains('div', 'login success')
   })
+
+  it('the user wants to remember password', () => {
+    cy.visit('https://shapershifter.onrender.com')
+    //cy.get('form')
+      cy.get('[id=remember-email]').type('john.smith@gmail.com')
+      cy.get('[id=remember-username]').type('smithy')
+
+
+    cy.get('[value="Remember Password"]').click()
+    cy.contains('div', 'pass: thisisapassword123')
+  })
+
+
+
   
 })
