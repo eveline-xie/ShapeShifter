b1667302ea33cdfb4d496f9d5c79d93d1a4c177f
Youni Chen <chenyouni0306@gmail.com>
2023-04-08 18:15:41 -0400

    login screen

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,11 +3,13 @@ import NavBar from "./NavBar"
 import HomeScreen from "./HomeScreen"
 import LoginScreen from "./LoginScreen"
 import MapCard from "./MapCard"
+import SignupScreen from "./SignupScreen"
 
 export{
     WelcomeScreen,
     NavBar,
     HomeScreen,
     LoginScreen,
+    SignupScreen,
     MapCard
 }
