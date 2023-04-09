dbfbb1b22ef4ab90f5649e412cdfe4ca7b3d798f
achen5401 <achen5401@gmail.com>
2023-04-09 14:09:23 -0400

    .

--- client/src/components/MapCard.js
+++ client/src/components/MapCard.js
@@ -5,22 +5,19 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import DeleteModal from './DeleteModal';
+import { useNavigate } from 'react-router-dom';
 
 export default function MapCard(props) {
-  var owner=""
+
+  let navigate = useNavigate();
+  var owner = ""
   // if(published){
   //   owner = <Typography variant="body2" color="text.secondary">
   //         owner name
   //       </Typography>
   // }
-  var leftButton =  <Button variant="contained" sx={{ maxWidth: 100 }} style={{
-        borderRadius: 50,
-        backgroundColor: "#FFE484",
-        padding: "7px 34px",
-        margin:"10px 10px",
-        fontSize: "13px",
-        color: "#000000"
-    }} >Edit</Button>
+
   // if(published){
   //   leftButton =  <Button variant="contained" sx={{ maxWidth: 100 }} style={{
   //       borderRadius: 50,
@@ -31,35 +28,56 @@ export default function MapCard(props) {
   //       color: "#000000"
   //   }} >View</Button>
   // }
- async function handleDeleteList(event, id) {
-        event.stopPropagation();
-        props.setOpenDelete(true);
-        // store.markListForDeletion(id);
-    }
+  async function handleDeleteList(event, id) {
+    event.stopPropagation();
+    props.setOpenDelete(true);
+    // store.markListForDeletion(id);
+  }
 
-     async function handleExport(event, id) {
-        event.stopPropagation();
-        props.setOpenExport(true);
-    }
-     async function handleFork(event, id) {
-       event.stopPropagation();
-       props.setOpenFork(true);
-     }
-  var deleteButton=   <Button variant="contained" sx={{ maxWidth: 100 }} style={{
-        borderRadius: 50,
-        backgroundColor: "#FFE484",
-        padding: "7px 34px",
-        margin:"10px 10px",
-        fontSize: "13px",
-        color: "#000000"
-    }} onClick={(event) => {
-                            handleDeleteList(event)
-                        }}>Delete</Button>
+  async function handleExport(event, id) {
+    event.stopPropagation();
+    props.setOpenExport(true);
+  }
+  async function handleFork(event, id) {
+    event.stopPropagation();
+    props.setOpenFork(true);
+  }
+
+  async function handleEditMap() {
+    console.log("Test");
+    navigate('/createmap');
+  }
+  var leftButton = <Button variant="contained" sx={{ maxWidth: 100 }} style={{
+    borderRadius: 50,
+    backgroundColor: "rgba(255,228,132, 0.4)",
+    padding: "7px 34px",
+    margin: "10px 10px",
+    fontSize: "13px",
+    color: "#FFE484",
+  }} onClick={handleEditMap}>Edit</Button>
+
+  var deleteButton = <Button variant="contained" sx={{ maxWidth: 100 }} style={{
+    borderRadius: 50,
+    backgroundColor: "rgba(255,228,132, 0.4)",
+    padding: "7px 34px",
+    margin: "10px 10px",
+    fontSize: "13px",
+    color: "#FFE484"
+  }} onClick={(event) => {
+    handleDeleteList(event)
+  }}>Delete</Button>
   // if(published){
   //   deleteButton=""
   // }
   return (
-    <Card sx={{ maxWidth: 325, borderRadius: "30px", backgroundColor: 'grey'}} id="mapcard">
+    <Card sx={{
+      maxWidth: 325, borderRadius: "30px",
+      backgroundColor: 'grey',
+      backgroundImage: `url(${"cardBackground.png"})`,
+      backgroundSize: 'cover',
+      position: 'absolute',
+      left: 0
+    }} id="mapcard">
       <CardMedia
         component="img"
         alt="green iguana"
@@ -68,7 +86,7 @@ export default function MapCard(props) {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div"
-        color="white" fontFamily="Kadwa" sx={{fontSize: 30, fontWeight: 'bold'}}>
+          color="white" fontFamily="Kadwa" sx={{ fontSize: 30, fontWeight: 'bold' }}>
           North America
         </Typography>
         {/* if published, add owner */}
@@ -102,11 +120,11 @@ export default function MapCard(props) {
             sx={{ maxWidth: 100 }}
             style={{
               borderRadius: 50,
-              backgroundColor: "#FFE484",
+              backgroundColor: "rgba(255,228,132, 0.4)",
               padding: "7px 34px",
               margin: "10px 10px",
               fontSize: "13px",
-              color: "#000000",
+              color: "#FFE484",
             }}
             onClick={(event) => {
               handleExport(event);
