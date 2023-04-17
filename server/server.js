b31d93f54e4288159090f92704d3a218d18cd80c
achen5401 <achen5401@gmail.com>
2023-04-17 00:30:32 -0400

    .

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://shapershifter.onrender.com/",
     credentials: true,
   })
 );
