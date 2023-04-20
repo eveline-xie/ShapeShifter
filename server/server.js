49d5894456b369750a58e5da2e6ecb2198f9191a
achen5401 <achen5401@gmail.com>
2023-04-19 22:39:14 -0400

    deploy to render

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    //origin: "https://shapershifter.onrender.com",
+    origin: "https://shapershifter.onrender.com",
+    //origin: "http://localhost:3000",
     credentials: true,
   })
 );
