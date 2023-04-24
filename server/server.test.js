92b3be3f1997a6a996327c2336a4c0970fa166fb
achen5401 <achen5401@gmail.com>
2023-04-24 14:25:43 -0400

    .

--- server/server.test.js
+++ server/server.test.js
@@ -1,231 +1,107 @@
 // run npm test
 const request = require("supertest"); // Import supertest
-const server = require("./server"); // Import the server object
-const mongoose = require("mongoose");
-const {
-  default: HomeScreen,
-} = require("./ShapeShifter/client/src/components/screens/HomeScreen");
+const server = require("./server") // Import the server object
+const mongoose = require('mongoose')
 
-beforeAll((done) => {
-  done();
-});
+beforeAll(done => {
+    done()
+  })
 
-afterEach((done) => {
-  // afterEach function is provided by Jest and executes once all tests are finished
-  server.close(); // We close the server connection once all tests have finished
-  done();
-});
+afterEach(done => { // afterEach function is provided by Jest and executes once all tests are finished
+    server.close() // We close the server connection once all tests have finished
+    done()
+})
 
 describe("Auth Tests", () => {
-  test("POST /auth/signup", (done) => {
-    request(server)
-      .post("/auth/signup")
-      // .expect("Content-Type", /json/)
-      .send({
-        firstName: "Stormyyyyy",
-        lastName: "Stormyyyy",
-        username: "stormyiscute",
-        email: "eveline.yang.xie@gmail.com",
-        password: "password",
-        passwordVerify: "password",
-      })
-      // .expect(200)
-      // .expect((res) => {
-      //   res.data.message = "Success";
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
+    test("POST /auth/signup", (done) => {
+      request(server)
+        .post("/auth/signup")
+        // .expect("Content-Type", /json/)
+        .send({
+          firstName: "Stormyyyyy",
+          lastName: "Stormyyyy",
+          username: "stormyiscute",
+          email: "stormy@stormy",
+          password: "password"
+        })
+        //.expect(200)
+        // .expect((res) => {
+        //     res.data.message = "Success"
+        // })
+        .end((err, res) => {
+            if (err) return done(err);
+           
+            return done();
+          });
+    
+        // Even more logic goes here
+    });
 
-        return done();
+    test("POST /auth/login", (done) => {
+        request(server)
+          .post("/auth/login")
+          // .expect("Content-Type", /json/)
+          .send({
+            email: "stormy@stormy",
+            password: "password"
+          })
+          //.expect(200)
+          // .expect((res) => {
+          //     res.data.message = "Success"
+          // })
+          .end((err, res) => {
+              if (err) return done(err);
+             
+              return done();
+            });
+      
+          // Even more logic goes here
       });
 
-    // Even more logic goes here
-  });
-
-  test("POST /auth/login", (done) => {
-    request(server)
-      .post("/auth/login")
-      // .expect("Content-Type", /json/)
-      .send({
-        email: "stormy@stormy",
-        password: "password",
-      })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
+      test("GET /auth/remember-password", (done) => {
+        request(server)
+          .get("/auth/remember-password")
+          // .expect("Content-Type", /json/)
+          .send({
+            email: "stormy@stormy",
+            username: "stormyiscute",
+          })
+          //.expect(200)
+          // .expect((res) => {
+          //     res.data.message = "Success"
+          // })
+          .end((err, res) => {
+              if (err) return done(err);
+             
+              return done();
+            });
+      
+          // Even more logic goes here
       });
 
-    // Even more logic goes here
-  });
-
-  test("GET /auth/remember-password", (done) => {
-    request(server)
-      .get("/auth/remember-password")
-      // .expect("Content-Type", /json/)
-      .send({
-        email: "stormy@stormy",
-        username: "stormyiscute",
-      })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
+      test("PUT /auth/change-password", (done) => {
+        request(server)
+          .put("/auth/change-password")
+          // .expect("Content-Type", /json/)
+          .send({
+            username: "stormyiscute",
+            password: "password",
+            newPassword: "newpassword",
+          })
+          //.expect(200)
+          // .expect((res) => {
+          //     res.data.message = "Success"
+          // })
+          .end((err, res) => {
+              if (err) return done(err);
+             
+              return done();
+            });
+      
+          // Even more logic goes here
       });
 
-    // Even more logic goes here
+      afterAll( async () =>{
+        await mongoose.connection.close()
+    })
   });
-
-  test("PUT /auth/updatepassword", (done) => {
-    request(server)
-      .put("/auth/updatepassword")
-      // .expect("Content-Type", /json/)
-      .send({
-        username: "stormyiscute",
-        password: "password",
-      })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
-      });
-
-    // Even more logic goes here
-  });
-
-  test("GET /auth/user", (done) => {
-    request(server)
-      .get("/auth/user")
-      // .expect("Content-Type", /json/)
-      .send({
-        email: "stormy@stormy",
-      })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
-      });
-
-    // Even more logic goes here
-  });
-
-  test("GET /load-user-maps-no-geojson", (done) => {
-    request(server)
-      .get("/load-user-maps-no-geojson")
-      // .expect("Content-Type", /json/)
-      .send({
-        userId: "643ca30a841c13d1ed5f04d6",
-      })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
-      });
-
-    // Even more logic goes here
-  });
-
-  test("GET /load-user-maps-no-geojson", (done) => {
-    request(server)
-      .get("/load-user-maps-no-geojson")
-      // .expect("Content-Type", /json/)
-      .send({
-        userId: "643ca30a841c13d1ed5f04d6",
-      })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
-      });
-
-    // Even more logic goes here
-  });
-
-  test("GET /load-user-maps", (done) => {
-    request(server)
-      .get("/load-user-maps")
-      // .expect("Content-Type", /json/)
-      .send({
-        userId: "643ca30a841c13d1ed5f04d6",
-      })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
-      });
-
-    // Even more logic goes here
-  });
-
-  test("GET /map/:id", (done) => {
-    request(server)
-      .get("/map/643c0face5b6c7bbe08d1c89")
-      // .expect("Content-Type", /json/)
-      //  .send({
-      //    userId: "643ca30a841c13d1ed5f04d6",
-      //  })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
-      });
-
-    // Even more logic goes here
-  });
-
-  test("DELETE /map/:id", (done) => {
-    request(server)
-      .delete("/map/643c0face5b6c7bbe08d1c89")
-      // .expect("Content-Type", /json/)
-      //  .send({
-      //    userId: "643ca30a841c13d1ed5f04d6",
-      //  })
-      //.expect(200)
-      // .expect((res) => {
-      //     res.data.message = "Success"
-      // })
-      .end((err, res) => {
-        if (err) return done(err);
-
-        return done();
-      });
-
-    // Even more logic goes here
-  });
-
-  afterAll(async () => {
-    await mongoose.connection.close();
-  });
-});
