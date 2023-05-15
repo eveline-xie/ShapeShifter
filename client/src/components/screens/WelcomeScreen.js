f556d6e42471e86935800ce1521cf0e513863076
Youni Chen <chenyouni0306@gmail.com>
2023-05-15 14:23:59 -0400

    .

--- client/src/components/screens/WelcomeScreen.js
+++ client/src/components/screens/WelcomeScreen.js
@@ -15,8 +15,9 @@ export default function SplashScreen() {
   const { auth } = useContext(AuthContext);
   function handleGuest(event) {
     console.log("guest log in rn");
-    if (!auth.loggedIn) navigate("/communityguest");
-    else navigate("/community");
+    // if (!auth.loggedIn) 
+    navigate("/communityguest");
+    // else navigate("/community");
 
     //auth.guestLogin();
   }
